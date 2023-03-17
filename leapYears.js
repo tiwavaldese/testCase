b268a84a0b96048038a7f7fa910d7d40705d07a5
tiwavaldese <tiwaval7@gmail.com>
2023-03-17 15:27:35 +0100

    leapYears.js

---
+++ leapYears.js
@@ -0,0 +1,9 @@
+function leapYears(){
+    if('2000 is a leap year  '){
+        return true
+    }
+    else if( '1985 is not a leap year'){
+        return false
+    }
+}
+module.exports = leapYears
